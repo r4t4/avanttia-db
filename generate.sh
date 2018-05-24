555a71349a25e9c1090502e05615f0a4db2c25e1
Claudio MAUBERT <rata@moullo.xyz>
2018-05-23 21:33:35 -0500

    generating a JS index & at least having the sequelize library as dependency

--- generate.sh
+++ generate.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
+javascript="./js"
+typescript="./ts"
+
 # forcing the .env... sorry... there are many ways to skin a cat
 source .env
 # generate JavaScript
-sequelize-auto -h $AVANTTIA_DB_HOST -d $AVANTTIA_DB_NAME -u $AVANTTIA_DB_USER -x $AVANTTIA_DB_PASS -p $AVANTTIA_DB_PORT  --dialect mysql -o ./js -C
+sequelize-auto -h $AVANTTIA_DB_HOST -d $AVANTTIA_DB_NAME -u $AVANTTIA_DB_USER -x $AVANTTIA_DB_PASS -p $AVANTTIA_DB_PORT  --dialect mysql -o ./js
 # generate TypeScript
-sequelize-auto -h $AVANTTIA_DB_HOST -d $AVANTTIA_DB_NAME -u $AVANTTIA_DB_USER -x $AVANTTIA_DB_PASS -p $AVANTTIA_DB_PORT  --dialect mysql -o ./ts -z -C
+sequelize-auto -h $AVANTTIA_DB_HOST -d $AVANTTIA_DB_NAME -u $AVANTTIA_DB_USER -x $AVANTTIA_DB_PASS -p $AVANTTIA_DB_PORT  --dialect mysql -o ./ts -z
+
+# construct a JavaScript index with all the tables
+index="$javascript/index.js"
+# clean it up
+truncate -s 0 $index
+
+# create the JS file
+echo "module.exports = {" >> $index
+for js in $javascript/*.js; do
+  model=$( echo $js | cut -d '/' -f 3 | cut -d '.' -f 1 )
+  echo "  $model: require('./$model')," >> $index
+done
+echo ""}"" >> $index
+
+echo 'all good... ciao'
