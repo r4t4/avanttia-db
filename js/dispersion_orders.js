2405c9373553e59189ac38345ea4d45fbc98c3f3
Claudio MAUBERT <rata@moullo.xyz>
2018-05-24 11:45:21 -0500

    cyclic refernces

--- js/dispersion_orders.js
+++ js/dispersion_orders.js
@@ -19,10 +19,10 @@ module.exports = function(sequelize, DataTypes) {
     payment_id: {
       type: DataTypes.INTEGER(10).UNSIGNED,
       allowNull: false,
-      references: {
-        model: 'payments',
-        key: 'id'
-      }
+//      references: {
+//        model: 'payments',
+//        key: 'id'
+//      }
     },
     parent_id: {
       type: DataTypes.INTEGER(10).UNSIGNED,
